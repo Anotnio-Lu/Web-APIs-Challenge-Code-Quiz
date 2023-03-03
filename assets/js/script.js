d924f2244996172f1f29cbc641e68854e0ae2857
Antonio Lu <Antonioraylu@outlook.com>
2023-03-04 04:14:14 +1100

    Updated JS: Created function to check for correct input and to set timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,63 @@
 var startB = document.querySelector(".start-button");
 var introContainer = document.querySelector(".intro-containter");
 var questionCard = document.querySelector(".question-container");
+var timeEl = document.querySelector(".time");
 var answer = document.getElementById("answer-container");
 var alertMessage = document.querySelector(".alert");
 
 
+var secondsLeft = 75;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+
+        secondsLeft--;
+        
+        timeEl.textContent = secondsLeft ;
+
+        if(secondsLeft === 0) {
+        secondsLeft = secondsLeft + 1
+        clearInterval(timerInterval);
+        timeEl.textContent = secondsLeft
+        return
+        }
+
+    }, 1000);
+}
+
+
 function questionOne(){
     answer.addEventListener("click", function(event){
-
-    let element = event.target;
-    let correctAnswer = element.getAttribute("id");
-
-    if(correctAnswer == 'answer-one'){
-        alertMessage.textContent = "Correct!"
-    } else {
-        alertMessage.textContent = "Incorrect!"
-    }
+        correctAnswerOnly(event, "answer-one");
         return
     })
     return
 }
 
 
+function correctAnswerOnly(select, input){
+    var element = select.target;
+    var correctAnswer = element.getAttribute("id");
+
+    if(correctAnswer == input){
+        alertMessage.textContent = "Correct!"
+        setTimeToClearText()
+        timeEl.textContent = secondsLeft
+    } else {
+        alertMessage.textContent = "Incorrect!"
+        setTimeToClearText()
+        secondsLeft -= 15;
+        timeEl.textContent = secondsLeft
+    }
+   return
+}
+
 
 function start(){
     introContainer.setAttribute("style", "display: none;")
     questionCard.setAttribute("style", "display: block;")
+    timeEl.textContent = secondsLeft
+    setTime()
     questionOne()
 }
 
